f9284bf19daddec44b3ec8f07342b177fd0923aa
ahimmelstoss <ahimmelstoss@gmail.com>
2013-12-01 16:40:11 -0500

    cleanup

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -1,16 +1,5 @@
-//js function that gets the sum data from the server and renders it into the sum div
-//ruby route/path that returns the data needed
-
-//periodically have the browser check and reload again
-
+// function that fetches the expense data from the server and renders json through the users controller; this is test function to then do the same thing for the whole feed. it will be messy.
 function fetchSumData(){
-  //users confirmed friends expenses and their sum total
-  // 1. Fetch the data that we need from a route, "/feed_sum".  The data is a json object that looks like this:
-  //   {"num_expenses": 14, "expenses_sum": 132.40}
-  // 2. With that json object, render HTML into the "sum" div.
-  // For now, we hard-code the user.
-  // TODO: Do not hard-code the user.
-  var user = "ahimmelstoss";
   $.get('/users/'+user+'/feed_sum', function(json){
     var num_expenses = json.num_expenses;
     var expenses_sum = json.expenses_sum;
@@ -21,3 +10,5 @@ function fetchSumData(){
 $(document).ready(fetchSumData);
 
 window.setInterval(fetchSumData, 7000);
+
+function 
