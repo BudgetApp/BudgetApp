1c110e83c12fd00a063ba5a52812e9074323ad59
Logan Hasson <logan.hasson@gmail.com>
2013-12-04 10:39:45 -0500

    Update seed file and add clearfix to main well on user show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,57 +3,98 @@
 # Do not hesitate to tweak this to your needs
 
 Category.create!([
-  { :title => "Dining Out", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Groceries", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Alcohol", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Take-Out", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Public Transportation", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Cabs/Taxis", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Clothing", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Out of Town Travel", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Cell Phone", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Rent", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Utilities", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Books", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Gym", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" },
-  { :title => "Misc.", :created_at => "2013-11-28 02:10:40", :updated_at => "2013-11-28 02:10:40" }
+  { :title => "Dining Out", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Groceries", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Alcohol", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Take-Out", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Public Transportation", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Cabs/Taxis", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Clothing", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Out of Town Travel", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Cell Phone", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Rent", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Utilities", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Books", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Gym", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" },
+  { :title => "Misc.", :created_at => "2013-11-26 21:17:06", :updated_at => "2013-11-26 21:17:06" }
 ])
 
 
 
 Expense.create!([
-  { :user_id => 2, :category_id => 3, :amount => 4050, :created_at => "2013-11-28 02:14:23", :updated_at => "2013-11-28 02:14:23" },
-  { :user_id => 2, :category_id => 13, :amount => 12000, :created_at => "2013-11-28 02:14:37", :updated_at => "2013-11-28 02:14:37" },
-  { :user_id => 2, :category_id => 7, :amount => 10000, :created_at => "2013-11-28 02:24:08", :updated_at => "2013-11-28 02:24:08" },
-  { :user_id => 2, :category_id => 5, :amount => 11200, :created_at => "2013-11-28 02:47:41", :updated_at => "2013-11-28 02:47:41" },
-  { :user_id => 2, :category_id => 4, :amount => 2050, :created_at => "2013-11-28 02:47:51", :updated_at => "2013-11-28 02:47:51" },
-  { :user_id => 2, :category_id => 8, :amount => 38500, :created_at => "2013-11-28 02:48:07", :updated_at => "2013-11-28 02:48:07" },
-  { :user_id => 2, :category_id => 6, :amount => 3245, :created_at => "2013-11-28 02:48:17", :updated_at => "2013-11-28 02:48:17" },
-  { :user_id => 2, :category_id => 4, :amount => 1632, :created_at => "2013-11-28 02:48:29", :updated_at => "2013-11-28 02:48:29" },
-  { :user_id => 2, :category_id => 3, :amount => 1898, :created_at => "2013-11-28 02:48:39", :updated_at => "2013-11-28 02:48:39" },
-  { :user_id => 2, :category_id => 2, :amount => 3367, :created_at => "2013-11-28 02:48:48", :updated_at => "2013-11-28 02:48:48" }
+  { :user_id => 2, :category_id => 1, :amount => 1200, :created_at => "2013-12-02 17:20:22", :updated_at => "2013-12-02 17:20:22" },
+  { :user_id => 2, :category_id => 1, :amount => 1100, :created_at => "2013-12-02 17:21:14", :updated_at => "2013-12-02 17:21:14" },
+  { :user_id => 2, :category_id => 1, :amount => 1100, :created_at => "2013-12-02 17:21:48", :updated_at => "2013-12-02 17:21:48" },
+  { :user_id => 2, :category_id => 1, :amount => 1200, :created_at => "2013-12-02 17:22:02", :updated_at => "2013-12-02 17:22:02" },
+  { :user_id => 2, :category_id => 1, :amount => 1100, :created_at => "2013-12-02 17:22:49", :updated_at => "2013-12-02 17:22:49" },
+  { :user_id => 2, :category_id => 1, :amount => 1100, :created_at => "2013-12-02 17:23:01", :updated_at => "2013-12-02 17:23:01" },
+  { :user_id => 2, :category_id => 1, :amount => 1200, :created_at => "2013-12-02 17:23:53", :updated_at => "2013-12-02 17:23:53" },
+  { :user_id => 2, :category_id => 1, :amount => 1200, :created_at => "2013-12-02 17:24:48", :updated_at => "2013-12-02 17:24:48" },
+  { :user_id => 2, :category_id => 1, :amount => 1122300, :created_at => "2013-12-02 17:25:14", :updated_at => "2013-12-02 17:25:14" },
+  { :user_id => 2, :category_id => 1, :amount => 12300, :created_at => "2013-12-02 17:25:32", :updated_at => "2013-12-02 17:25:32" },
+  { :user_id => 2, :category_id => 1, :amount => 12500, :created_at => "2013-12-02 17:44:20", :updated_at => "2013-12-02 17:44:20" },
+  { :user_id => 2, :category_id => 1, :amount => 12300, :created_at => "2013-12-02 17:46:17", :updated_at => "2013-12-02 17:46:17" },
+  { :user_id => 2, :category_id => 1, :amount => 12300, :created_at => "2013-12-02 17:46:31", :updated_at => "2013-12-02 17:46:31" },
+  { :user_id => 2, :category_id => 1, :amount => 20000, :created_at => "2013-12-02 17:47:13", :updated_at => "2013-12-02 17:47:13" },
+  { :user_id => 2, :category_id => 1, :amount => 12300, :created_at => "2013-12-02 17:51:24", :updated_at => "2013-12-02 17:51:24" },
+  { :user_id => 2, :category_id => 1, :amount => 1200, :created_at => "2013-12-02 17:51:50", :updated_at => "2013-12-02 17:51:50" },
+  { :user_id => 2, :category_id => 1, :amount => 100, :created_at => "2013-12-02 17:51:56", :updated_at => "2013-12-02 17:51:56" },
+  { :user_id => 2, :category_id => 1, :amount => 3300, :created_at => "2013-12-02 17:52:11", :updated_at => "2013-12-02 17:52:11" },
+  { :user_id => 2, :category_id => 1, :amount => 1200, :created_at => "2013-12-02 17:53:47", :updated_at => "2013-12-02 17:53:47" },
+  { :user_id => 2, :category_id => 1, :amount => 2300, :created_at => "2013-12-02 17:54:59", :updated_at => "2013-12-02 17:54:59" },
+  { :user_id => 2, :category_id => 1, :amount => 2300, :created_at => "2013-12-02 17:55:59", :updated_at => "2013-12-02 17:55:59" },
+  { :user_id => 2, :category_id => 1, :amount => 4500, :created_at => "2013-12-02 17:56:06", :updated_at => "2013-12-02 17:56:06" },
+  { :user_id => 2, :category_id => 1, :amount => 2676, :created_at => "2013-12-02 17:56:56", :updated_at => "2013-12-02 17:56:56" },
+  { :user_id => 2, :category_id => 1, :amount => 2300, :created_at => "2013-12-02 17:57:31", :updated_at => "2013-12-02 17:57:31" },
+  { :user_id => 2, :category_id => 1, :amount => 2213, :created_at => "2013-12-02 18:00:21", :updated_at => "2013-12-02 18:00:21" },
+  { :user_id => 2, :category_id => 1, :amount => 2300, :created_at => "2013-12-02 18:02:25", :updated_at => "2013-12-02 18:02:25" },
+  { :user_id => 2, :category_id => 1, :amount => 232400, :created_at => "2013-12-02 18:03:29", :updated_at => "2013-12-02 18:03:29" },
+  { :user_id => 2, :category_id => 6, :amount => 12300, :created_at => "2013-12-02 18:05:12", :updated_at => "2013-12-02 18:05:12" },
+  { :user_id => 2, :category_id => 1, :amount => 200, :created_at => "2013-12-02 18:08:58", :updated_at => "2013-12-02 18:08:58" },
+  { :user_id => 2, :category_id => 1, :amount => 2300, :created_at => "2013-12-02 18:16:42", :updated_at => "2013-12-02 18:16:42" },
+  { :user_id => 1, :category_id => 1, :amount => 1200, :created_at => "2013-12-03 22:24:48", :updated_at => "2013-12-03 22:24:48" },
+  { :user_id => 1, :category_id => 2, :amount => 1000, :created_at => "2013-12-03 22:24:57", :updated_at => "2013-12-03 22:24:57" },
+  { :user_id => 1, :category_id => 3, :amount => 2500, :created_at => "2013-12-03 22:25:05", :updated_at => "2013-12-03 22:25:05" },
+  { :user_id => 1, :category_id => 3, :amount => 26600, :created_at => "2013-12-03 22:25:11", :updated_at => "2013-12-03 22:25:11" },
+  { :user_id => 1, :category_id => 4, :amount => 1200, :created_at => "2013-12-03 22:41:09", :updated_at => "2013-12-03 22:41:09" },
+  { :user_id => 1, :category_id => 5, :amount => 5600, :created_at => "2013-12-03 22:41:15", :updated_at => "2013-12-03 22:41:15" },
+  { :user_id => 1, :category_id => 7, :amount => 2200, :created_at => "2013-12-03 22:41:24", :updated_at => "2013-12-03 22:41:24" },
+  { :user_id => 1, :category_id => 8, :amount => 9800, :created_at => "2013-12-03 22:41:29", :updated_at => "2013-12-03 22:41:29" },
+  { :user_id => 1, :category_id => 9, :amount => 200, :created_at => "2013-12-03 22:41:35", :updated_at => "2013-12-03 22:41:35" },
+  { :user_id => 1, :category_id => 10, :amount => 25000, :created_at => "2013-12-03 22:41:45", :updated_at => "2013-12-03 22:41:45" },
+  { :user_id => 1, :category_id => 11, :amount => 4376, :created_at => "2013-12-03 22:41:54", :updated_at => "2013-12-03 22:41:54" },
+  { :user_id => 1, :category_id => 12, :amount => 50000, :created_at => "2013-12-03 22:42:01", :updated_at => "2013-12-03 22:42:01" },
+  { :user_id => 1, :category_id => 13, :amount => 3000, :created_at => "2013-12-03 22:42:12", :updated_at => "2013-12-03 22:42:12" },
+  { :user_id => 1, :category_id => 14, :amount => 2300, :created_at => "2013-12-03 22:42:19", :updated_at => "2013-12-03 22:42:19" },
+  { :user_id => 1, :category_id => 1, :amount => 19900, :created_at => "2013-12-03 23:16:25", :updated_at => "2013-12-03 23:16:25" },
+  { :user_id => 1, :category_id => 12, :amount => 1276, :created_at => "2013-12-04 00:15:44", :updated_at => "2013-12-04 00:15:44" },
+  { :user_id => 1, :category_id => 6, :amount => 5500, :created_at => "2013-12-04 00:39:05", :updated_at => "2013-12-04 00:39:05" }
 ])
 
 
 
 Friendship.create!([
-  { :user_id => 2, :friend_id => 1, :created_at => "2013-11-28 02:13:38", :updated_at => "2013-11-28 02:13:38", :accepted => true },
-  { :user_id => 1, :friend_id => 2, :created_at => "2013-11-28 02:13:38", :updated_at => "2013-11-28 02:13:54", :accepted => true }
+  { :user_id => 1, :friend_id => 2, :created_at => "2013-12-02 17:19:59", :updated_at => "2013-12-02 17:19:59", :accepted => true },
+  { :user_id => 2, :friend_id => 1, :created_at => "2013-12-02 17:19:59", :updated_at => "2013-12-02 17:20:12", :accepted => true }
 ])
 
 
 
 User.create!([
-  { :name => "Amanda Himmelstoss", :email => "ahimmelstoss@gmail.com", :facebook_id => nil, :facebook_token => nil, :created_at => "2013-11-28 02:11:07", :updated_at => "2013-11-28 02:11:07", :remember_token => "e011b815fd1bd27ad806e70b8fba5bde9710f384", :provider => "facebook", :uid => "1343310232", :info => {"nickname"=>"ahimmelstoss", "email"=>"ahimmelstoss@gmail.com", "name"=>"Amanda Himmelstoss", "first_name"=>"Amanda", "last_name"=>"Himmelstoss", "image"=>"http://graph.facebook.com/1343310232/picture?type=square", "urls"=>{"Facebook"=>"https://www.facebook.com/ahimmelstoss"}, "location"=>"Brooklyn, New York", "verified"=>true}, :username => "ahimmelstoss" },
-  { :name => "Alex Hioreanu", :email => "hioreanu@gmail.com", :facebook_id => nil, :facebook_token => nil, :created_at => "2013-11-28 02:13:32", :updated_at => "2013-11-28 02:13:32", :remember_token => "d2901c8cb0173a6f6ad9b220a1d439accf75965a", :provider => "facebook", :uid => "1536072019", :info => {"nickname"=>"hioreanu", "email"=>"hioreanu@gmail.com", "name"=>"Alex Hioreanu", "first_name"=>"Alex", "last_name"=>"Hioreanu", "image"=>"http://graph.facebook.com/1536072019/picture?type=square", "urls"=>{"Facebook"=>"https://www.facebook.com/hioreanu"}, "location"=>"Brooklyn, New York", "verified"=>true}, :username => "hioreanu" }
+  { :name => "Logan Hasson", :email => "lhvio89@gmail.com", :facebook_id => nil, :facebook_token => nil, :created_at => "2013-12-02 17:09:12", :updated_at => "2013-12-02 17:09:12", :remember_token => "1969666db56f3afd9356ce10bf0cae71ee0d52b6", :provider => "facebook", :uid => "1401360047", :info => {"nickname"=>"logan.hasson", "email"=>"lhvio89@gmail.com", "name"=>"Logan Hasson", "first_name"=>"Logan", "last_name"=>"Hasson", "image"=>"http://graph.facebook.com/1401360047/picture?type=square", "urls"=>{"Facebook"=>"https://www.facebook.com/logan.hasson"}, "verified"=>true}, :username => "logan.hasson", :hashed_uid => "bc130d1f2464ba232f7fee6f60b49bb3fd868678" },
+  { :name => "Amanda Himmelstoss", :email => "ahimmelstoss@gmail.com", :facebook_id => nil, :facebook_token => nil, :created_at => "2013-12-02 17:19:51", :updated_at => "2013-12-02 17:19:51", :remember_token => "1138e8a95db1014431e0ed558629d21837487a2d", :provider => "facebook", :uid => "1343310232", :info => {"nickname"=>"ahimmelstoss", "email"=>"ahimmelstoss@gmail.com", "name"=>"Amanda Himmelstoss", "first_name"=>"Amanda", "last_name"=>"Himmelstoss", "image"=>"http://graph.facebook.com/1343310232/picture?type=square", "urls"=>{"Facebook"=>"https://www.facebook.com/ahimmelstoss"}, "location"=>"Brooklyn, New York", "verified"=>true}, :username => "ahimmelstoss", :hashed_uid => "a345a2003d4e5a0e84cedb1e90bd777b30cb1b71" }
 ])
 
 
 
 Vote.create!([
-  { :created_at => "2013-11-28 02:20:15", :updated_at => "2013-11-28 02:45:48", :vote_direction => true, :expense_id => 2, :user_id => 1 },
-  { :created_at => "2013-11-28 02:20:21", :updated_at => "2013-11-28 02:45:47", :vote_direction => false, :expense_id => 1, :user_id => 1 },
-  { :created_at => "2013-11-28 02:24:25", :updated_at => "2013-11-28 02:45:50", :vote_direction => false, :expense_id => 3, :user_id => 1 }
+  { :created_at => "2013-12-02 17:57:15", :updated_at => "2013-12-02 17:57:16", :vote_direction => false, :expense_id => 37, :user_id => 2 },
+  { :created_at => "2013-12-02 17:57:25", :updated_at => "2013-12-02 17:57:26", :vote_direction => false, :expense_id => 38, :user_id => 2 },
+  { :created_at => "2013-12-02 18:00:14", :updated_at => "2013-12-02 18:02:32", :vote_direction => false, :expense_id => 39, :user_id => 2 },
+  { :created_at => "2013-12-02 18:02:29", :updated_at => "2013-12-02 18:02:31", :vote_direction => false, :expense_id => 40, :user_id => 2 },
+  { :created_at => "2013-12-02 18:05:16", :updated_at => "2013-12-02 18:05:19", :vote_direction => false, :expense_id => 43, :user_id => 2 },
+  { :created_at => "2013-12-02 18:09:00", :updated_at => "2013-12-02 18:09:01", :vote_direction => false, :expense_id => 44, :user_id => 2 },
+  { :created_at => "2013-12-02 18:16:43", :updated_at => "2013-12-02 18:16:44", :vote_direction => false, :expense_id => 45, :user_id => 2 }
 ])
 
 
